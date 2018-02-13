2183229c32ec2cf5b90f4fdd1d41b78c49d11f4d
Ren Qian <qinaren1024@icloud.com>
2018-02-13 15:11:06 +0800

    debug

--- transfer/socket_server.py
+++ transfer/socket_server.py
@@ -17,24 +17,24 @@ class VideoStreamHandler(socketserver.BaseRequestHandler):
             count -= len(newbuf)
         return buf
 
-    # 接收图片
-    def recv_all(self, sock, count):
-        buf = b''
-        while count:
-            # 这里每次只接收一个字节的原因是增强python与C++的兼容性
-            # python可以发送任意的字符串，包括乱码，但C++发送的字符中不能包含'\0'，也就是字符串结束标志位
-            newbuf = sock.recv(1)
-            if not newbuf: return None
-            buf += newbuf
-            count -= len(newbuf)
-        return buf
+    # # 接收图片
+    # def recv_all(self, sock, count):
+    #     buf = b''
+    #     while count:
+    #         # 这里每次只接收一个字节的原因是增强python与C++的兼容性
+    #         # python可以发送任意的字符串，包括乱码，但C++发送的字符中不能包含'\0'，也就是字符串结束标志位
+    #         newbuf = sock.recv(1)
+    #         if not newbuf: return None
+    #         buf += newbuf
+    #         count -= len(newbuf)
+    #     return buf
 
     def handle(self):
         while True:
             length = self.recv_size(self.request, 16)  #首先接收来自客户端发送的大小信息
             print(length)
             if isinstance (length.decode(), str): #若成功接收到大小信息，进一步再接收整张图片
-                stringData = self.recv_all(self.request, int(length))
+                stringData = self.recv_size(self.request, int(length))
                 data = numpy.fromstring(stringData, dtype='uint8')
                 decimg=cv2.imdecode(data, 1)         #解码处理，返回mat图片
                 
