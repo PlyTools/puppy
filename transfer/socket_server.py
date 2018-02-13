187259e9ac1953e18e7b40e293a85c315cb13077
Ren Qian <qinaren1024@icloud.com>
2018-02-13 14:21:50 +0800

    debug

--- transfer/socket_server.py
+++ transfer/socket_server.py
@@ -18,7 +18,7 @@ class VideoStreamHandler(socketserver.BaseRequestHandler):
             length = self.recv_size(self.request, 1024) #首先接收来自客户端发送的大小信息
             print(length)
             if length: #若成功接收到大小信息，进一步再接收整张图片
-                streamData = self.recv_size(self.request, length)
+                streamData = self.recv_size(self.request, int(length.decode()))
                 data = numpy.fromstring(streamData, dtype='uint8')
                 decimg=cv2.imdecode(data, 1)         #解码处理，返回mat图片
                 
