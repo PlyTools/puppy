04187c637685a3929086232111ea33d750191109
Ren Qian <qinaren1024@icloud.com>
2018-02-23 00:31:31 +0800

    debug

--- compu_main.py
+++ compu_main.py
@@ -6,7 +6,8 @@ from config import config
 if __name__ == "__main__":
     # videoServer = SocketServer().TCPServer(config.compu_ip, config.video_port, VideoStreamHandler)
     paramsClient = SocketClient().TCPClient(config.raspi_ip, config.paras_port)
-    params = bku1.processImage(bku1.hoststr, bku1.M, bku1.initParams, bku1.refPos)
-    paramsClient.send(str(params).encode())
+    while True:
+        params = bku1.processImage(bku1.hoststr, bku1.M, bku1.initParams, bku1.refPos)
+        paramsClient.send(str(params).encode())
     
     # ultraServer = SocketServer().TCPServer(config.compu_ip, config.ultra_port, UltraStreamHandler)
