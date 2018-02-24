4bdbf9fbff6aace2003aa28f02509ec83d14db72
Ren Qian <qinaren1024@icloud.com>
2018-02-24 18:09:56 +0800

    debug

--- raspi_main.py
+++ raspi_main.py
@@ -4,6 +4,7 @@ from control.ultrasonic import Ultrasound
 from transfer.socket_client import SocketClient
 from transfer.socket_server import *
 import os
+import subprocess
 from config import config
 
 
@@ -13,11 +14,12 @@ if __name__ == "__main__":
     paramsServer = SocketServer().TCPServer(config.raspi_ip, config.paras_port, ParamsStreamHandler)
 
     # os.system('tools/remote_camera.sh')
+    sub = subprocess.Popen('tools/remote_camera.sh')
 
-    # create a client for video stream transmission
-    print("Create TCP Client thread to send video stream")
-    camera = Camera()
-    videoClient = SocketClient().TCPClient(config.compu_ip, config.video_port)
+    # # create a client for video stream transmission
+    # print("Create TCP Client thread to send video stream")
+    # camera = Camera()
+    # videoClient = SocketClient().TCPClient(config.compu_ip, config.video_port)
 
     # # create a client for ultrasound sensor data transmission
     # ultrasound = Ultrasound()
