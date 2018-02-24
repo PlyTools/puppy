3bd932c0ce46a1abe03f3f91f9c5cea7d8c91b6e
Ren Qian <qinaren1024@icloud.com>
2018-02-24 17:16:49 +0800

    debug

--- compu_main.py
+++ compu_main.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from transfer.socket_server import *
-import lane.laneline_coord_bku1 as bku1
+# import lane.laneline_coord_bku1 as bku1
 from config import config
 
 if __name__ == "__main__":
-    # videoServer = SocketServer().TCPServer(config.compu_ip, config.video_port, VideoStreamHandler)
+    videoServer = SocketServer().TCPServer(config.compu_ip, config.video_port, VideoStreamHandler)
 
-    paramsClient = SocketClient().TCPClient(config.raspi_ip, config.paras_port)
-    while True:
-        params = bku1.processImage(bku1.hoststr, bku1.M, bku1.initParams, bku1.refPos)
-        paramsClient.send(str(params).encode())
+    # paramsClient = SocketClient().TCPClient(config.raspi_ip, config.paras_port)
+    # while True:
+    #     params = bku1.processImage(bku1.hoststr, bku1.M, bku1.initParams, bku1.refPos)
+    #     paramsClient.send(str(params).encode())
     
     # ultraServer = SocketServer().TCPServer(config.compu_ip, config.ultra_port, UltraStreamHandler)
