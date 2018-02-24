1d05c45fa33370ab8a93f026c8871f9378d78a08
Ren Qian <qinaren1024@icloud.com>
2018-02-24 19:54:09 +0800

    debug

--- compu_main.py
+++ compu_main.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
 from transfer.socket_server import *
-import lane.laneline_coord_bku1 as bku1
 from config import config
 
 if __name__ == "__main__":
-    # videoServer = SocketServer().TCPServer(config.compu_ip, config.video_port, VideoStreamHandler)
-
-    paramsClient = SocketClient().TCPClient(config.raspi_ip, config.paras_port)
-    while True:
-        params = bku1.processImage(bku1.hoststr, bku1.M, bku1.initParams, bku1.refPos)
-        paramsClient.send(str(params).encode())
+    videoServer = SocketServer().TCPServer(config.compu_ip, config.video_port, VideoStreamHandler)
     
     # ultraServer = SocketServer().TCPServer(config.compu_ip, config.ultra_port, UltraStreamHandler)
--- raspi_main.py
+++ raspi_main.py
@@ -13,20 +13,20 @@ if __name__ == "__main__":
     print("Create TCP Server thread to receive params stream")
     paramsServer = SocketServer().TCPServer(config.raspi_ip, config.paras_port, ParamsStreamHandler)
 
-    # os.system('tools/remote_camera.sh')
-    sub = subprocess.Popen('tools/remote_camera.sh')
+    # # os.system('tools/remote_camera.sh')
+    # sub = subprocess.Popen('tools/remote_camera.sh')
 
-    # # create a client for video stream transmission
-    # print("Create TCP Client thread to send video stream")
-    # camera = Camera()
-    # videoClient = SocketClient().TCPClient(config.compu_ip, config.video_port)
+    # create a client for video stream transmission
+    print("Create TCP Client thread to send video stream")
+    camera = Camera()
+    videoClient = SocketClient().TCPClient(config.compu_ip, config.video_port)
 
-    # # # create a client for ultrasound sensor data transmission
-    # # ultrasound = Ultrasound()
-    # # ultraClient = SocketClient().TCPClient(config.compu_ip, config.ultra_port)
+    # # create a client for ultrasound sensor data transmission
+    # ultrasound = Ultrasound()
+    # ultraClient = SocketClient().TCPClient(config.compu_ip, config.ultra_port)
 
-    # print("Running...")
-    # # send data
-    # while True:
-    #     videoClient.send(camera.getFrameArray())
-    # #     ultraClient.send(ultrasound.get_distance())
+    print("Running...")
+    # send data
+    while True:
+        videoClient.send(camera.getFrameArray())
+    #     ultraClient.send(ultrasound.get_distance())
