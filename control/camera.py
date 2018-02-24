89b9098997d77d64a04b30144fc26d1204b15971
Ren Qian <qinaren1024@icloud.com>
2018-02-24 17:51:04 +0800

    debug

--- control/camera.py
+++ control/camera.py
@@ -18,12 +18,12 @@ class Camera:
     def getFrameArray(self):
         stream = io.BytesIO()
         self.camera.capture(stream, format="jpeg", use_video_port=True)
-        # frameArray = np.array(stream.getvalue())
         #~ frameArray = np.fromstring(stream.getvalue(), dtype = np.uint8)
         return stream.getvalue()
         
     def getFrame(self):
-        frameArray = self.getFrameArray()
+        streamValue = self.getFrameArray()
+        frameArray = np.array(streamValue)
         frame = cv2.imdecode(frameArray, 1)
         return frame
         
